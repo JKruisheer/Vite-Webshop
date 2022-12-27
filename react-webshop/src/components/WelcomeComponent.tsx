cd16431d21fc7294e62b6ec700090f4f9574183d
Jesse Kruisheer <70973567+JKruisheer@users.noreply.github.com>
2022-12-27 09:53:17 +0100

    Support env variables
    Had to take a look at the tsconfig files.

--- react-webshop/src/components/WelcomeComponent.tsx
+++ react-webshop/src/components/WelcomeComponent.tsx
@@ -6,9 +6,11 @@ import {DivStyles} from "../styles/DivStyles";
 
 export class WelcomeComponent extends Component {
     render () {
+
         return (
             <div style={DivStyles.WelcomeDivStyle}>
                 <Stack spacing={3} style={StackStyles.FlexCenter}>
+                    <Text fontSize='4xl'>Environment var: {import.meta.env.VITE_SOME_KEY}</Text>
                     <Text fontSize='4xl'>Welkom bij de XXX dag</Text>
                     <Text style={TextStyles.welcomeStyle}>
                         Vandaag gaan we een webshop bouwen
